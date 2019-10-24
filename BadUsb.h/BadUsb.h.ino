b2ec3e2ae5b4798f690f5085b295f29b251155bd
jamgrams <jamgrams@gmail.com>
2019-10-23 21:11:01 -0700

    Create BadUsb.h.ino
    not sure why it's an ino file but this is the basic code to load onto the badUSB

---
+++ BadUsb.h/BadUsb.h.ino
@@ -0,0 +1,19 @@
+#include <Keyboard.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+//Hello
+
+
+  Keyboard.begin();
+  Keyboard.print("z");
+  Keyboard.print("Hello");
+
+//Click on next or hide with OSAScript?
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
